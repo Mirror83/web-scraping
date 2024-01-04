e1ae28e097f8d75c4ce926b940e140bf2117ce18
Mirror83 <glenochieng045@gmail.com>
2024-01-04 07:37:45 +0300

    Added some experiments with beautiful soup

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-contents = []
+from bs4 import BeautifulSoup
 
-with open("website.html", encoding="utf-8") as website:
-    contents.extend(website.readlines())
+with open("website.html", encoding="utf-8") as file:
+    contents = file.read()
 
-print(contents)
+soup = BeautifulSoup(contents, features="html.parser")
+anchor_tags = soup.find_all(name="a")
+
+for tag in anchor_tags:
+    print(tag["href"])
+
+heading = soup.find_all(attrs={"class": "headings"})
+print(heading)
+
+company_url = soup.select_one("p a")
+print(company_url)
